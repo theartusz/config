a2b00ebd3fc84cdd82128e991dcc03a0d1cb3fce
Artur Ferfecki <artureczek_5@live.com>
2021-12-02 16:29:16 +0100

    removing some stuff

--- config.fish
+++ config.fish
@@ -13,12 +13,5 @@ alias kns=kubens
 #github user
 set -x GITHUB_USER theartusz
 
-#github user for terraform
-set -x TF_VAR_github_owner $GITHUB_USER
-
-#github token for terrafomr
-set -x TF_VAR_github_token $GITHUB_TOKEN
-
-
 # enable starship
 starship init fish | source
