bdfa99d854e90116bf823d93a0ba641db7a4ac93
Artur Ferfecki <artureczek_5@live.com>
2021-12-02 16:24:53 +0100

    removing some stuff

--- config.fish
+++ config.fish
@@ -19,8 +19,6 @@ set -x TF_VAR_github_owner $GITHUB_USER
 #github token for terrafomr
 set -x TF_VAR_github_token $GITHUB_TOKEN
 
-# npm token
-set -x NPM_TOKEN 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
 
 # enable starship
 starship init fish | source
