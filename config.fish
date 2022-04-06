b604933a5667902581f99daef13d46c93bfd22a2
Artur Ferfecki <40075318+theartusz@users.noreply.github.com>
2022-04-06 11:32:35 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,6 +3,7 @@ alias t=terraform # custom alias for terraform
 alias kx=kubectx # custom kubectx alias
 alias kns=kubens # custom kubens alias
 set -x GITHUB_USER theartusz #github user
+set -x TF_VAR_GITHUB_OWNER theartusz
 
 # enable starship
 starship init fish | source
