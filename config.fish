715dab33363168fc0b1d5c9057205846bdafd600
Artur Ferfecki <artur.ferfecki@sikt.no>
2023-01-24 09:57:40 +0100

    adding alias for sed

--- config.fish
+++ config.fish
@@ -4,12 +4,13 @@ alias kx=kubectx # custom kubectx alias
 alias kns=kubens # custom kubens alias
 alias python=/usr/bin/python3 # make python3 default
 alias pip=/usr/bin/pip3 # make pip3 default
+alias sed=gsed # used gnu-sed instead of mac sed
 set -x GITHUB_USER theartusz #github user
 set -x TF_VAR_GITHUB_OWNER theartusz
 set -x STARSHIP_CONFIG ~/repos/toolkit/starship.toml
 
 # mac
-#set PATH /opt/homebrew/bin $PATH 
+#set PATH /opt/homebrew/bin $PATH
 
 # enable starship
 starship init fish | source
